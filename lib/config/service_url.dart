64b944b8e4c85dc181598d151862ceb06254e4d3
Jack Wang <864655687@qq.com>
2020-06-12 17:18:05 +0800

    修改了部分代码

---
+++ lib/config/service_url.dart
@@ -0,0 +1,4 @@
+const serviceUrl= 'https://time.geekbang.org/serv/v2/explore/list/';
+const servicePath={
+  'homePageContent':serviceUrl+"wxmini/homePageContent",//商店首页信息
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_shop/service/service_method.dart';
 //https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=大胸美女
 class HomePage extends StatefulWidget {
   @override
@@ -7,72 +8,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = new TextEditingController();
-  String showText='欢迎你来到美好人间';
+  String showText = '还没有请求数据';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getHomePageContent().then((val){
+      setState(() {
+        showText=val.toString();
+      });
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(appBar: AppBar(title:Text('美好人间')),
-      body: Container(child: Column(
-        children: <Widget>[
-          TextField(
-            controller: typeController,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(10.0),
-              labelText: '美女类型',
-              helperText: '请输入你喜欢的类型'
-            ),
-            autofocus: false,
-          ),
-
-          RaisedButton(onPressed: _choiceAction,
-          child: Text('选择完毕'),
-          ),
-        Text(
-        showText,
-        overflow:TextOverflow.ellipsis,
-        maxLines:1
-        )
-        ],
-
-
-      ),),
-      ),
-      
-    );
+  return Scaffold(appBar: AppBar(title:Text('数据请求')),
+  body: SingleChildScrollView(
+    child:Text(showText)
+  ),
+  );
   }
-  void _choiceAction(){
-    print("开始选择你喜欢的类型");
-    if(typeController.text.toString()==''){
-      showDialog(context: context,
-      builder:(context)=>AlertDialog(title: Text('美女类型为空'),)
-      );
-    }else{
-      getHttp(typeController.text.toString()).then((val){
-        print(val);
-        setState(() {
-          showText=val['data']['name'].toString();
-         //showText="haha";
-        });
-      });
-    }
-  }
-
-  Future getHttp(String TypeText) async{
-    try{
-      Response response;
-      var data={'name':TypeText};
-      response=await Dio().post('https://www.easy-mock.com/mock/5ee32b0c3cfbe27ca22162a3/example/post_aiyin',
-      queryParameters:data
-      );
-      print("hahahha--------"+response.data);
-      return response.data;
-
-    }catch(e){
-     return print(e);
-    }
-  }
-
-
-
 }
